e88bc58e3cda940482c6ff14e90a6a65aa9892a2
jcg-school <jcaelynschool@outlook.com>
2025-05-24 19:05:06 -0400

    Core requirements 1 & 2 met, before changing code

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        //practicing asking for input without jumping to the next line
+        Console.Write("Enter your grade percentage");
+        string inputPercentage = Console.ReadLine();
+
+        //practicing turning a user input (which must be a string intitially) into an integer, assuming the use actually entered integers
+        int gradePercentage = int.Parse(inputPercentage);
+
+        if (gradePercentage >= 90)
+        {
+            Console.WriteLine($"Your grade is a A");
+        }
+        else if (gradePercentage >= 80)
+        {
+            Console.WriteLine($"Your grade is a B");
+        }
+        else if (gradePercentage >= 70)
+        {
+            Console.WriteLine($"Your grade is a C");
+        }
+        else if (gradePercentage >= 60)
+        {
+            Console.WriteLine($"Your grade is a D");
+        }
+        else
+        {
+            Console.WriteLine($"Your grade is a F");
+        }
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("You have passed the class.");
+        }
+        else
+        {
+            Console.WriteLine("You have failed the class.");
+        }
+
     }
 }
