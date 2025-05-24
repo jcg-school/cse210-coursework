aa1583ff362d1f2175775d349f0107c738cd661f
jcg-school <jcaelynschool@outlook.com>
2025-05-24 18:44:34 -0400

    Exercise 1 completed.

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("What is your first name?");
+        string firstName = Console.ReadLine();
+
+        Console.WriteLine("What is your last name?");
+        string lastName = Console.ReadLine();
+
+        //Practicing clearing the terminal
+        Console.Clear();
+
+        //practicing two types of including variables in string output, the $ recognition format and the + concatenation
+        Console.WriteLine($"Your name is {lastName}, {firstName}" + " " + lastName + ".");
+
+        //Practicing adding a blank line for spacing & readability
+        Console.WriteLine("");
+        //Using Console.Write would NOT jump to a new line afterwards.
+
+        //Reminder to self, never run "DuBug Anyway" as that runs the previous version of code.
     }
 }
